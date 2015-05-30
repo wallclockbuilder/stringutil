caa9a2c71883912ded2b765fc874aa390cc536c0
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-05-30 14:07:07 +0000

    Fix spacing for documentation to work.

--- xstrings/upcase.go
+++ xstrings/upcase.go
@@ -7,7 +7,6 @@ import (
 // Upcase returns a copy of string s
 // with all lowercase letters replaced
 // with their uppercase counterparts.
-
 func Upcase(s string) string {
 	return strings.ToUpper(s)
 }
