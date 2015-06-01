85d985e59a166adfae6303e3fd90a6bdb1bb0ee3
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-06-01 06:53:31 +0000

    Add pseudo regex to Include()

--- xstrings/include.go
+++ xstrings/include.go
@@ -1,11 +1,28 @@
 package xstrings
 
 import (
+	"regexp"
 	"strings"
 )
 
 // Include returns true if str contains the given string
 // or character.
 func Include(s string, substr string) bool {
-	return strings.Contains(s, substr)
+	r := strings.Contains(s, substr)
+	if firstChar(substr) == "?" {
+		r, _ = regexp.MatchString(patternRe2(substr), s)
+	}
+	return r
+}
+
+func firstChar(s string) string {
+	return string(s[0:1])
+}
+
+// patternRe2 takes in ruby user-friendly
+// pseudo regex and returns RE2 compliant regex pattern.
+// pseudo regex pattern: "?h"
+// RE2 compliant regex pattern: "h?"
+func patternRe2(s string) string {
+	return s[1:] + s[0:1]
 }
--- xstrings/include_test.go
+++ xstrings/include_test.go
@@ -9,4 +9,5 @@ func TestInclude(t *testing.T) {
 	assert := assert.New(t)
 	assert.Equal(true, Include("hello", "lo"))
 	assert.Equal(false, Include("hello", "ol"))
+	assert.Equal(true, Include("hello", "?h"))
 }
