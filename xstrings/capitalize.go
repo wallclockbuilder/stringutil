d2cd8f20df42659f08bc7ab097f4d3db470a74ca
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-06-04 07:31:23 +0000

    Add documentation to capitalize.

--- xstrings/capitalize.go
+++ xstrings/capitalize.go
@@ -4,6 +4,10 @@ import (
 	"strings"
 )
 
+// Capitalize returns a new string
+// with the first character converted
+// to uppercase and the remainder to lowercase.
+// Note: case conversion is effective only in ASCII region.
 func Capitalize(s string) string {
 	return strings.ToUpper(s)
 }
