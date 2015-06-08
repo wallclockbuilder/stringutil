aa0e51c47b0e9d9896d3e016af56207d92a1d4cc
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-06-08 05:59:06 +0000

    Add example to Downcase

--- xstrings/downcase_test.go
+++ xstrings/downcase_test.go
@@ -1,9 +1,15 @@
 package xstrings
 
 import (
+	"fmt"
 	"github.com/wallclockbuilder/testify/assert"
 	"testing"
 )
+
+func ExampleDowncase() {
+	fmt.Println(Downcase("hEllO"))
+	// Output: hello
+}
 
 func TestDowncase(t *testing.T) {
 	assert := assert.New(t)
