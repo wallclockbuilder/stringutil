a9d29b7d11871f06c3a220d84ab022c82ea19ece
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-06-11 07:35:40 +0000

    Add examples to Chop()

--- xstrings/chop_test.go
+++ xstrings/chop_test.go
@@ -1,9 +1,19 @@
 package xstrings
 
 import (
+	"fmt"
 	"github.com/wallclockbuilder/testify/assert"
 	"testing"
 )
+
+func ExampleChop() {
+	fmt.Println(Chop("Keep it simple"))
+	fmt.Println(Chop("Keep it simple\n"))
+	fmt.Println(Chop("Developer happiness FTW\r\n"))
+	// Output: Keep it simpl
+	// Keep it simple
+	// Developer happiness FTW
+}
 
 func TestChop(t *testing.T) {
 	assert.Equal(t, "string", Chop("string\r\n"))
