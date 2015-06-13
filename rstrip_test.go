13628e4411a7f99bc5630b1e67ca7316fcd797f6
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-06-13 05:40:57 +0000

    Usecase for rstrip.

--- rstrip_test.go
+++ rstrip_test.go
@@ -1,9 +1,15 @@
 package gstrings
 
 import (
+	"fmt"
 	"github.com/wallclockbuilder/testify/assert"
 	"testing"
 )
+
+func ExampleRstrip(){
+	fmt.Println(Rstrip("   hello   "))
+	// Output:   hello
+}
 
 func TestRstrip(t *testing.T) {
 	assert := assert.New(t)
