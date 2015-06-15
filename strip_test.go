047937a86b254803e4aa21c479fa526270fcceb4
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-06-15 05:39:27 +0000

    Add usecase for Strip(s)

--- strip_test.go
+++ strip_test.go
@@ -1,9 +1,17 @@
 package gstrings
 
 import (
+	"fmt"
 	"github.com/wallclockbuilder/testify/assert"
 	"testing"
 )
+
+func ExampleStrip(){
+	fmt.Println(Strip("   hello   "))
+	fmt.Println(Strip("\tgoodbye\r\n"))
+	// Output: hello
+	// goodbye
+}
 
 func TestStrip(t *testing.T) {
 	assert := assert.New(t)
