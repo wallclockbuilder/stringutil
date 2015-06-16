00fe8edb4dee89dd9def512ddcc89da00ff9edb6
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-06-16 08:23:53 +0000

    Sample usecase added for Prepend()

--- prepend_test.go
+++ prepend_test.go
@@ -1,9 +1,15 @@
 package gstrings
 
 import (
+	"fmt"
 	"github.com/wallclockbuilder/testify/assert"
 	"testing"
 )
+
+func ExamplePrepend(){
+	fmt.Println(Prepend("planet", "capt. "))
+	// Output: capt. planet
+}
 
 func TestPrepend(t *testing.T) {
 	assert := assert.New(t)
