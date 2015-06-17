5c95358d982de4f9a165f0a6e88c1f1c15d1fccf
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-06-17 05:58:23 +0000

    Example usecase added to documentation for start_with

--- start_with_test.go
+++ start_with_test.go
@@ -1,9 +1,15 @@
 package gstrings
 
 import (
+	"fmt"
 	"github.com/wallclockbuilder/testify/assert"
 	"testing"
 )
+
+func ExampleStart_with(){
+	fmt.Println(Start_with("hello", "hell"))
+	// Output: true
+}
 
 func TestStart_with(t *testing.T) {
 	assert := assert.New(t)
