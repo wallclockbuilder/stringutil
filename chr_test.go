238c858d8340a435bcd7f26ccf2e8bb72331a62d
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-06-19 05:45:02 +0000

    Add example to Chr

--- chr_test.go
+++ chr_test.go
@@ -1,9 +1,15 @@
 package gstrings
 
 import (
+	"fmt"
 	"github.com/wallclockbuilder/testify/assert"
 	"testing"
 )
+
+func ExampleChr(){
+	fmt.Println(Chr("abcde"))
+	// Output: a
+}
 
 func TestChr(t *testing.T) {
 	assert := assert.New(t)
