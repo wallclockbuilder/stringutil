6ffa3e1fe7320ba02767bd2140abb16caf7020e2
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-06-29 03:22:46 +0000

    Add swapcase()

---
+++ swapcase.go
@@ -0,0 +1,21 @@
+package gstrings
+
+import (
+  "unicode"
+  "strings"
+)
+
+// Swapcase returns a copy of str
+// with uppercase alphabetic characters converted to lowercase and
+// lowercase characters converted to uppercase.
+// Note: case conversion is effective only in ASCII region.
+func Swapcase(s string) string {
+  return strings.Map(swapRuneCase, s)
+}
+
+func swapRuneCase(r rune) rune{
+  if (unicode.IsLower(r)){
+    return unicode.ToUpper(r)
+  }
+  return unicode.ToLower(r)
+}
