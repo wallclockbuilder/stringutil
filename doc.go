fb59bb4ff0a95bce78b7de5a76f3290a69b80b42
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-07-14 03:53:04 +0000

    simplify docs. Let this work. We can add the samples later.

--- doc.go
+++ doc.go
@@ -1,14 +1,5 @@
-/*
-Package gstrings provides common sense
-string manipulation methods from ruby.
-     package main
-     import "github.com/wallclockbuilder/gstrings"
 
-     func main() {
-       gstrings.Capitalize("abcde")          #=> "ABCDE"
-       gstrings.Reverse("stressed")          #=> "desserts"
-       gstrings.Swapcase("Hello")            #=> "hELLO"
-     }
-*/
+// Package gstrings provides common sense
+// string manipulation methods from ruby.
 
 package gstrings
