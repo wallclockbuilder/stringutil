32d342d3b7243079a1953fe211a9781314e2070a
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-07-14 03:41:51 +0000

    Fix for #36 Move package documentation to doc.go

--- chop.go
+++ chop.go
@@ -1,14 +1,3 @@
-// Package gstrings provides common sense
-// string manipulation methods from ruby.
-//     package main
-//     import "github.com/wallclockbuilder/gstrings"
-//
-//     func main() {
-//       gstrings.Capitalize("abcde")          #=> "ABCDE"
-//       gstrings.Reverse("stressed")          #=> "desserts"
-//       gstrings.Swapcase("Hello")            #=> "hELLO"
-//     }
-
 package gstrings
 
 import (
---
+++ doc.go
@@ -0,0 +1,13 @@
+package gstrings
+/*
+Package gstrings provides common sense
+string manipulation methods from ruby.
+     package main
+     import "github.com/wallclockbuilder/gstrings"
+
+     func main() {
+       gstrings.Capitalize("abcde")          #=> "ABCDE"
+       gstrings.Reverse("stressed")          #=> "desserts"
+       gstrings.Swapcase("Hello")            #=> "hELLO"
+     }
+*/
