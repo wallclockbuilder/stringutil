ae650b9d1eba25f8ea9bd7274c0e1ef7b97cf70e
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-07-14 04:04:29 +0000

    Update

--- doc.go
+++ doc.go
@@ -3,6 +3,7 @@ Package gstrings provides common sense
 string manipulation methods from ruby.
      package main
      import "github.com/wallclockbuilder/gstrings"
+
      func main() {
        gstrings.Capitalize("abcde")          #=> "ABCDE"
        gstrings.Reverse("stressed")          #=> "desserts"
