3b59be468ec6b6a63113a7afd8f228b3af908e56
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-07-14 02:41:55 +0000

    Add code sample to godoc. Fix for #34

--- chop.go
+++ chop.go
@@ -1,5 +1,15 @@
 // Package gstrings provides common sense
 // string manipulation methods from ruby.
+
+// package main
+// import "github.com/wallclockbuilder/gstrings"
+//
+// func main() {
+//   gstrings.Capitalize("abcde")          #=> "ABCDE"
+//   gstrings.Reverse("stressed")          #=> "desserts"
+//   gstrings.Swapcase("Hello")            #=> "hELLO"
+// }
+
 package gstrings
 
 import (
